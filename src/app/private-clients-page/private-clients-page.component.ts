3bb49489a3a930771eb3263e809dfc313e18c439
BehzodRj <behzodfarishta@gmail.com>
2022-05-03 14:06:04 +0500

    some changes

--- src/app/private-clients-page/private-clients-page.component.ts
+++ src/app/private-clients-page/private-clients-page.component.ts
@@ -7,12 +7,13 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./private-clients-page.component.scss']
 })
 export class PrivateClientsPageComponent implements OnInit {
-  // tarifsData: any = [
-  //   {name: 'Nets 1', price: '99', text: 'Бесплатное IPTV в подарок на 1 месяц'},
-  //   {name: 'Nets 2', price: '125', text: 'Бесплатное IPTV в подарок на 1 месяц'},
-  //   {name: 'Nets 3', price: '225', text: 'Бесплатное IPTV в подарок на 3 месяц'},
-  //   {name: 'Nets 4', price: '375', text: 'Бесплатное IPTV в подарок на 4 месяц'},
-  // ]
+  tarifsData: any = [
+    {id: 1, name: 'Nets 1', speed: 'Скорость до 1 Mbit/s', price: '99', li_1: 'Оборудование в аренду', li_2: 'Бесплатное IPTV в подарок на 1 месяц', li_3: 'Бесплатное подключение', li_4: 'Безлимитный трафик', li_5: 'Доступ к локальным ресурсам до 100мбит'},
+    {id: 2, name: 'Nets 2', speed: 'Скорость до 5 Mbit/s', price: '125', li_1: 'Оборудование в аренду', li_2: 'Бесплатное IPTV в подарок на 1 месяц', li_3: 'Бесплатное подключение', li_4: 'Безлимитный трафик', li_5: 'Доступ к локальным ресурсам до 100мбит'},
+    {id: 3, name: 'Nets 3', speed: 'Скорость до 10 Mbit/s', price: '225', li_1: 'Оборудование в аренду', li_2: 'Бесплатное IPTV в подарок на 2 месяц', li_3: 'Бесплатное подключение', li_4: 'Безлимитный трафик', li_5: 'Доступ к локальным ресурсам до 100мбит'},
+    {id: 4, name: 'Nets 4', speed: 'Скорость до 20 Mbit/s', price: '375', li_1: 'Оборудование в аренду', li_2: 'Бесплатное IPTV в подарок на 2 месяц', li_3: 'Бесплатное подключение', li_4: 'Безлимитный трафик', li_5: 'Доступ к локальным ресурсам до 100мбит'},
+  ]
+  tarifsName: any
   showModalConnection = false
   clrTech = 0
 
@@ -20,8 +21,14 @@ export class PrivateClientsPageComponent implements OnInit {
 
   ngOnInit() {}
 
+  connectionTarifs(id: number) {
+    this.showModalConnection = true
+    this.tarifsName = this.tarifsData.filter( (res: any) =>  res.id == id)[0].name
+  }
+
   gpon(elem: any) {
     this.clrTech = 1
+    console.log(elem.target.value); 
   }
 
   ethernet(elem: any) {
