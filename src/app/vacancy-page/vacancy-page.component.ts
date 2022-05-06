e42d4bc90555c91cfed7f0a1cc4b6cbe3cd05593
BehzodRj <behzodfarishta@gmail.com>
2022-05-06 13:32:15 +0500

    many changes

--- src/app/vacancy-page/vacancy-page.component.ts
+++ src/app/vacancy-page/vacancy-page.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vacancy-page.component.scss']
 })
 export class VacancyPageComponent implements OnInit {
+  vacancyData: any = [
+    {id: 1, img: './assets/img/vacancy.svg', title: 'Специалист по подключениям', description: 'Не просто специалист…а Мега-коммуникабельный, активный, способный уладить любую проблему супер-герой!'},
+    {id: 2, img: './assets/img/manager.svg', title: 'Менеджер В2В', description: 'Осуществляет активные продажи, поиск новых клиентов В2В сегмента, процесс продвижение новых услуг для юридических лиц.'},
+    {id: 3, img: './assets/img/operator.svg', title: 'Оператор', description: 'Не просто оператор…а Мега-коммуникабельный, активный, способный уладить любую проблему супер-герой!'}
+  ]
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit() {}
 
 }
--- src/app/vacancy-special/vacancy-special.component.ts
+++ src/app/vacancy-special/vacancy-special.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-vacancy-special',
@@ -7,13 +8,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./vacancy-special.component.scss']
 })
 export class VacancySpecialComponent implements OnInit {
+  vacancySpecialData: any = [
+    {id: 1, img: './assets/img/vacancy.svg', title: 'Специалист по подключениям'},
+    {id: 2, img: './assets/img/manager.svg', title: 'Менеджер В2В'},
+    {id: 3, img: './assets/img/operator.svg', title: 'Оператор'}
+  ]
+  fullVacancyData: any
   vacancyAddForm!:FormGroup
   letterAddForm!: FormGroup
-
   modalShow = false
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    
     this.vacancyAddForm = new FormGroup({
       surname: new FormControl('', Validators.required),
       name: new FormControl('', Validators.required),
@@ -23,6 +31,10 @@ export class VacancySpecialComponent implements OnInit {
     })
     this.letterAddForm = new FormGroup({
       letter: new FormControl('',Validators.required)
+    })
+
+    this.route.params.subscribe( (param: any) => {
+      this.fullVacancyData = this.vacancySpecialData.filter( (res: any) => res.id == param.id)[0]
     })
   }
 
